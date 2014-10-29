2fcccaa514442c5bc8be63c483a55800802f5586
Philippe Schwyter <pschwyter90@gmail.com>
2014-10-29 11:27:48 -0400

    added ability to edit contacts

--- crm.rb
+++ crm.rb
@@ -17,9 +17,55 @@ get "/contacts/new" do
 	erb :new_contact
 end
 
+get "/contacts/edit/:id" do
+	id = params[:id].to_i
+	puts params
+	contact_to_edit = $rolodex.find_contact(id)[0]
+	puts contact_to_edit
+	$rolodex.set_contact(contact_to_edit)
+	erb :edit_contact
+end
+
+
+get "/contacts/delete/:id" do
+	id = params[:id].to_i
+	contact_to_delete = $rolodex.find_contact(id)[0]
+	$rolodex.set_contact(contact_to_delete)
+	erb :delete_contact
+end
+
 post "/contacts" do
 	puts params
 	new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
 	$rolodex.add_contact(new_contact)
 	redirect to('/contacts')
 end
+
+post "/contacts_edit" do
+	puts params
+	$rolodex.edit_contact(params[:first_name],params[:last_name],params[:email],params[:note])
+	redirect to('/contacts')
+end
+
+# delete "/contacts" do
+# 	puts params
+# 	contact_to_delete = $rolodex.find_contact(params)
+# end
+
+
+
+#Only for debugging
+post "/contacts_generate" do
+	new_contact = Contact.new("Phil", "Schwyter", "pschwyter90@gmail.com", "student")
+	$rolodex.add_contact(new_contact)
+	new_contact = Contact.new("Bob", "Smith", "bob@gmail.com", "student")
+	$rolodex.add_contact(new_contact)
+	new_contact = Contact.new("Jerry", "Jackson", "jerry@gmail.com", "student")
+	$rolodex.add_contact(new_contact)
+	new_contact = Contact.new("Frank", "Fenster", "frank@gmail.com", "student")
+	$rolodex.add_contact(new_contact)
+	new_contact = Contact.new("Laura", "Lincoln", "laura@gmail.com", "student")
+	$rolodex.add_contact(new_contact)
+	redirect to('/contacts')
+end
+
--- rolodex.rb
+++ rolodex.rb
@@ -1,5 +1,5 @@
 class Rolodex
-	attr_reader :contacts
+	attr_reader :contacts, :selected_contact
 
 	def initialize 
 		@contacts = []
@@ -10,5 +10,21 @@ class Rolodex
 		contact.id = @id
 		@id += 1
 		@contacts << contact
+	end
+
+	def find_contact(id)
+		@contacts.select { |contact| contact.id == id }
+	end
+
+	def set_contact(contact)
+		@selected_contact = contact
+		@selected_contact
+	end
+
+	def edit_contact(first_name,last_name,email,note)
+		@selected_contact.first_name = first_name
+		@selected_contact.last_name = last_name
+		@selected_contact.email = email
+		@selected_contact.note = note
 	end
 end
